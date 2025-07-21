38c42895df448f793d37e7bb2656a0ede6cd5c7f
anhng1106 <ntvanh1106@gmail.com>
2025-07-21 12:26:52 +0300

    Replace submodule unicafe with regular folder

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+  return (
+    <>
+      <div>
+        <h1>give feedback</h1>
+
+        <button onClick={handleGoodClick} style={{ marginRight: "10px" }}>
+          good
+        </button>
+        <button onClick={handleNeutralClick} style={{ marginRight: "10px" }}>
+          neutral
+        </button>
+        <button onClick={handleBadClick}>bad</button>
+
+        <h1>statistic</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
