b37f8c4b5761cdf4e13118fdce376f356d648c05
anhng1106 <ntvanh1106@gmail.com>
2025-07-21 23:19:03 +0300

    Completed ex 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,21 +2,25 @@ import { useState } from "react";
 import React from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleAddPerson = (event) => {
     event.preventDefault();
-    if (newName.trim() === "") {
-      alert("Name cannot be empty");
+    if ((newName && newNumber).trim() === "") {
+      alert("Name and number cannot be empty");
       return;
     }
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      const personObject = { name: newName };
+      const personObject = { name: newName, number: newNumber };
       setPersons(persons.concat(personObject));
       setNewName("");
+      setNewNumber("");
     }
   };
 
@@ -32,6 +36,13 @@ const App = () => {
           />
         </div>
         <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(event) => setNewNumber(event.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit" onClick={handleAddPerson}>
             add
           </button>
@@ -40,7 +51,9 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map((person, index) => (
-          <p key={index}>{person.name}</p>
+          <p key={index}>
+            {person.name} {person.number}
+          </p>
         ))}
       </div>
     </div>
