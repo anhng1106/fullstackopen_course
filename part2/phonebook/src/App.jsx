4d4f251788faa3c7a81c6b7bebd16f2c4b88210b
anhng1106 <ntvanh1106@gmail.com>
2025-07-21 23:04:45 +0300

    Completed ex 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import React from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    if (newName.trim() === "") {
+      alert("Name cannot be empty");
+      return;
+    } else {
+      const personObject = { name: newName };
+      setPersons(persons.concat(personObject));
+      setNewName("");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAddPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, index) => (
+          <p key={index}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
