a4eefff46b563cbd53ab7619a391b981d4399064
anhng1106 <ntvanh1106@gmail.com>
2025-07-21 12:35:30 +0300

    Completed ex 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import React from "react";
 
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positive = total === 0 ? 0 : (good / total) * 100;
 
   const handleGoodClick = () => setGood(good + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
@@ -26,6 +31,9 @@ function App() {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {average.toFixed(2)}</p>
+        <p>positive {positive.toFixed(2)} %</p>
       </div>
     </>
   );
