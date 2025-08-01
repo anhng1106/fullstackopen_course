42408b2b5dfcdcd3aab99049f43806ab928b33cb
anhng1106 <ntvanh1106@gmail.com>
2025-08-01 22:26:43 +0300

    re created repo

--- practice/api_ex/api_practice/src/App.jsx
+++ practice/api_ex/api_practice/src/App.jsx
@@ -5,15 +5,20 @@ function App() {
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
   const [selectedUser, setSelectedUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
+
     userService
       .getUsers()
       .then((userData) => {
         setUsers(userData);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching users:", error);
+        setLoading(false);
       });
   }, []);
 
@@ -35,25 +40,32 @@ function App() {
           value={search}
           onChange={handleSearchChange}
         />
-        <ul>
-          {filteredUsers.map((user) => (
-            <li key={user.id}>
-              {user.name} ({user.email}){" "}
-              <button onClick={() => setSelectedUser(user)}>Show</button>
-            </li>
-          ))}
-        </ul>
 
-        {selectedUser && (
-          <div>
-            <h3>Selected User Info</h3>
-            <p>Name: {selectedUser.name}</p>
-            <p>Email: {selectedUser.email}</p>
-            <p>Phone: {selectedUser.phone}</p>
-            <p>Website: {selectedUser.website}</p>
-            <p>Company: {selectedUser.company?.name}</p>
-            <button onClick={() => setSelectedUser(null)}>Close</button>
-          </div>
+        {loading ? (
+          <p> Loading users...</p>
+        ) : (
+          <>
+            <ul>
+              {filteredUsers.map((user) => (
+                <li key={user.id}>
+                  {user.name} ({user.email}){" "}
+                  <button onClick={() => setSelectedUser(user)}>Show</button>
+                </li>
+              ))}
+            </ul>
+
+            {selectedUser && (
+              <div>
+                <h3>Selected User Info</h3>
+                <p>Name: {selectedUser.name}</p>
+                <p>Email: {selectedUser.email}</p>
+                <p>Phone: {selectedUser.phone}</p>
+                <p>Website: {selectedUser.website}</p>
+                <p>Company: {selectedUser.company?.name}</p>
+                <button onClick={() => setSelectedUser(null)}>Close</button>
+              </div>
+            )}
+          </>
         )}
       </div>
     </>
